904837b530d919feab8ca17d51af042dac1ab2d6
Cherviatsova <chervyatsovaelena2@gmail.com>
2022-03-31 14:57:21 +0300

    added eslint

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import MovieList from '../MovieList/MovieList';
 import { useEffect, useState } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
